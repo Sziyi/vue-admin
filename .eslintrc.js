7b02f3f6bfdab96b4815f603a3dbe22033020cf0
Sziyi <3275157006@qq.com>
2022-07-05 09:16:15 +0800

    feat(下载element): 引入element

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'error', // 默认
+    quotes: 'warn', // 修改为警告
+    quotes: 'off', // 修改不校验
+    'space-before-function-paren': 'off'
   }
 }
